e486b729cf1837df87ab9316f56e081e774c769e
Gratien <gratiensossouzin1@gmail.com>
2023-05-12 22:40:51 +0100

    100-puppet_ssh_config

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,9 @@
+# ssh file configuration config
+file_line { 'disable password login':
+  path => '/etc/ssh/ssh_config',
+  line => 'PasswordAuthentication no',
+}
+file_line { 'add path to find the keys':
+  path => '/etc/ssh/ssh_config',
+  line => 'IdentityFile ~/.ssh/school',
+}
